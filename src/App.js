f477378a4fc200803d7b348a12cc4fa29b840db1
msc20246 <78804051+msc20246@users.noreply.github.com>
2022-04-13 21:04:06 +0100

    Add files via upload
    edit profile

--- src/App.js
+++ src/App.js
@@ -1,102 +1,31 @@
-import React, { useState, useEffect } from 'react'
-import fire from './fire'
-import Login from './Login'
-import Hero from './Hero';
-import './App.css';
+import React, { useState } from 'react';
 
-const App = () => {
-  const [user, setUser] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [emailError, setEmailError] = useState('')
-  const [passwordError, setPasswordError] = useState('')
-  const [hasAccount, setHasAccount] = useState('')
 
-  const clearInputs = () => {
-    setEmail('')
-    setPassword('')
-  }
-
-  const clearErrors = () => {
-    setEmailError('')
-    setPasswordError('')
-  }
-
-  const handleLogin = () => {
-    clearErrors();
-    fire
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .catch(err => {
-        switch (err.code) {
-          case "auth/invalid-email":
-          case "auth/user-disabled":
-          case "auth/user-not-found":
-            setEmailError(err.message);
-            break;
-          case "auth/wrong-password":
-            setPasswordError(err.message);
-            break;
-        }
-      })
-  }
-
-  const handleSignup = () => {
-    clearErrors();
-    fire
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .catch(err => {
-        switch (err.code) {
-          case "auth/email-already-in-use":
-          case "auth/invaild-email":
-            setEmailError(err.message);
-            break;
-          case "auth/weak-password":
-            setPasswordError(err.message);
-            break;
-        }
-      })
-  }
-
-  const handleLogout = () => {
-    fire.auth().signOut();
-  }
-
-  const authListener = () => {
-    fire.auth().onAuthStateChanged(user => {
-      if (user) {
-        clearInputs();
-        setUser(user);
-      } else {
-        setUser("")
-      }
-    })
-  }
-
-  useEffect(() => {
-    authListener();
-  }, [])
-
+function App() {
   return (
-    <div className="App">
-      {user ? (
-        <Hero handleLogout={handleLogout} />
-      ) : (
-        <Login
-          email={email}
-          setEmail={setEmail}
-          password={password}
-          setPassword={setPassword}
-          handleLogin={handleLogin}
-          handleSignup={handleSignup}
-          hasAccount={hasAccount}
-          setHasAccount={setHasAccount}
-          emailError={emailError}
-          passwordError={passwordError}
-        />
-      )}
-    </div>
+    <form>
+      <div className="form-inner">
+          <h2>Edit Profile</h2>
+          <div className="form-group">
+              <label htmlFor="firstname">First Name:</label>
+              <input type="text" name="firstname" id="firstname"/>  
+          </div>
+          <div className="form-group">
+              <label htmlFor="lastname">Last Name:</label>
+              <input type="text" name="lastname" id="lastname"/>  
+          </div> 
+          <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input type="email" name="email" id="email"/>  
+          </div> 
+          <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input type="password" name="password" id="password"/>  
+          </div>
+          <input type = "submit" value= "Save Changes"/>     
+      </div>
+
+    </form>
   );
 }
 
