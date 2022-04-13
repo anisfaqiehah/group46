7565b97cf53e7cb92b8cc66b14533b56294c10b2
Asad <44686097+asaddkn@users.noreply.github.com>
2022-04-13 17:36:13 +0100

    Add files via upload

---
+++ Navigation/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { NavLink } from "react-router-dom";
+
+
+import Navigation from './Navigation'
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
