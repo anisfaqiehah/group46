22d15d73fead0338cf0655bc32f05f0bfc75d25f
Asad <44686097+asaddkn@users.noreply.github.com>
2022-04-13 23:12:41 +0100

    Asad -- semi compilation

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
 import React, { useState } from 'react';
 
+import Navigation from './Navigation'
+import Login from './Login'
+import Hero from './Hero'
+import MessageForm from './ChatMsg'
+import Message from './Message'
 
 function App() {
   return (
-    <form>
-      <div className="form-inner">
-          <h2>Edit Profile</h2>
-          <div className="form-group">
-              <label htmlFor="firstname">First Name:</label>
-              <input type="text" name="firstname" id="firstname"/>  
-          </div>
-          <div className="form-group">
-              <label htmlFor="lastname">Last Name:</label>
-              <input type="text" name="lastname" id="lastname"/>  
-          </div> 
-          <div className="form-group">
-              <label htmlFor="email">Email:</label>
-              <input type="email" name="email" id="email"/>  
-          </div> 
-          <div className="form-group">
-              <label htmlFor="password">Password:</label>
-              <input type="password" name="password" id="password"/>  
-          </div>
-          <input type = "submit" value= "Save Changes"/>     
-      </div>
+    <div className="App">
+      <Navigation />
+      <Login />
+      <Hero />
 
-    </form>
+    </div>
   );
 }
 
---
+++ src/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navigation = () => (
+  <div>
+  <nav class="navbar_home">
+    <a class="active" href="">Home</a>
+    <a href="">Calendar</a>
+    <a href="">Profile</a>
+    <a href="./Login">Login</a>
+  </nav>
+  </div>
+);
+
+export default Navigation;
